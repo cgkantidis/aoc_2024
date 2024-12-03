b5c943645d66311cdbd340c1a76f1db77b5af82e
Christos Gkantidis <cgkantid@proton.me>
2024-12-04 00:42:25 +0100

    Add solutions for day 2 part 2

--- src/d02p2.cpp
+++ src/d02p2.cpp
@@ -1,9 +1,10 @@
 #include <algorithm> // std::ranges::sort
 #include <fmt/core.h>
-#include <fmt/ranges.h>
 #include <fstream>
-#include <string>
 #include <ranges>
+#include <string>
+
+#include <libassert/assert.hpp>
 
 #include "utility.hpp"
 
@@ -13,71 +14,63 @@ are_increasing(std::size_t a, std::size_t b) {
 }
 
 bool
-is_safe_int(std::vector<std::size_t> const &levels) {
-  bool error_found{false};
-  std::size_t skip_next{0};
-  for (auto window : std::ranges::slide_view(levels, 3)) {
-    //fmt::println("{} {} {}", window[0], window[1], window[2]);
-    if (skip_next != 0) {
-      //fmt::println("skip");
-      --skip_next;
-      continue;
-    }
-    bool inc_01 = are_increasing(window[0], window[1]);
-    bool inc_12 = are_increasing(window[1], window[2]);
-    if (inc_01 && inc_12) {
-      continue;
-    }
-    if (!(inc_01 || inc_12)) {
-      //fmt::println("false *");
-      return false;
-    }
-    if (error_found) {
-      //fmt::println("false **");
-      return false;
-    }
-    //fmt::print("*");
-    if (inc_12) {
-      error_found = true;
-      //fmt::println("[{}]{}{}", window[0], window[1], window[2]);
-      // we're skipping the window[0]
-      continue;
-    }
-    bool inc_02 = are_increasing(window[0], window[2]);
-    if (inc_02) {
-      error_found = true;
-      //fmt::println("{}[{}]{}", window[0], window[1], window[2]);
-      // we're skipping the window[1]
-      skip_next = 1;
-      continue;
-    }
-  }
-  //fmt::println("true");
-  return true;
+is_safe_int(std::ranges::range auto &&levels) {
+  return std::ranges::all_of(std::views::slide(levels, 2), [](auto window) {
+    return are_increasing(window[0], window[1]);
+  });
 }
-//bool
-//is_safe_int(std::vector<std::size_t> const &levels) {
-//  for (auto window : std::ranges::slide_view(levels, 2)) {
-//    if (!are_increasing(window[0], window[1])) {
-//      return false;
-//    }
-//  }
-//  return true;
-//}
 
 bool
-is_safe(std::vector<std::size_t> &levels) {
-  if (is_safe_int(levels)) {
-    return true;
+is_safe_int(std::ranges::range auto &&levels, std::size_t pivot) {
+  auto left_end = std::next(levels.begin(), pivot);
+  auto left = std::ranges::subrange(levels.begin(), left_end);
+  auto right_begin = std::next(left_end, 1);
+  auto right = std::ranges::subrange(right_begin, levels.end());
+
+  if (left.empty()) {
+    return is_safe_int(right);
   }
-  std::ranges::reverse(levels);
-  return is_safe_int(levels);
+  if (right.empty()) {
+    return is_safe_int(left);
+  }
+  return is_safe_int(left) && is_safe_int(right)
+         && are_increasing(levels[pivot - 1], levels[pivot + 1]);
+}
+
+bool
+is_safe(std::ranges::range auto &&levels) {
+  for (std::size_t pivot = 0; pivot < levels.size(); ++pivot) {
+    if (is_safe_int(levels, pivot)
+        || is_safe_int(std::views::reverse(levels), pivot)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void
+tests() {
+  ASSERT(is_safe(std::vector<std::size_t>({{7, 6, 4, 2, 1}})));
+  ASSERT(!is_safe(std::vector<std::size_t>({{1, 2, 7, 8, 9}})));
+  ASSERT(!is_safe(std::vector<std::size_t>({{9, 7, 6, 2, 1}})));
+  ASSERT(is_safe(std::vector<std::size_t>({{1, 3, 2, 4, 5}})));
+  ASSERT(is_safe(std::vector<std::size_t>({{8, 6, 4, 4, 1}})));
+  ASSERT(is_safe(std::vector<std::size_t>({{1, 3, 6, 7, 9}})));
+  ASSERT(is_safe(std::vector<std::size_t>({{1, 5, 6}})));
+  ASSERT(is_safe(std::vector<std::size_t>({{1, 2, 6}})));
+  ASSERT(is_safe(std::vector<std::size_t>({{1, 2, 3, 4}})));
+  ASSERT(is_safe(std::vector<std::size_t>({{1, 4, 7, 10}})));
+  ASSERT(is_safe(std::vector<std::size_t>({{1, 5, 7, 10}})));
+  ASSERT(is_safe(std::vector<std::size_t>({{5, 6, 5, 7}})));
 }
 
 int
 main(int argc, char const *const *argv) {
-  if (argc != 2) {
-    fmt::println(stderr, "usage: {} input.txt", argv[0]);
+  tests();
+
+  auto args = std::span(argv, size_t(argc));
+  if (args.size() != 2) {
+    fmt::println(stderr, "usage: {} input.txt", args[0]);
     return 1;
   }
 
@@ -94,11 +87,8 @@ main(int argc, char const *const *argv) {
     std::ranges::transform(tokens, std::back_inserter(levels), str_to_int);
     if (is_safe(levels)) {
       ++total;
-    } else {
-      fmt::println("{}", levels);
     }
   }
   fmt::println("{}", total);
   return 0;
 }
-
